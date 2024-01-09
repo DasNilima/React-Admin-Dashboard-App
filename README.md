af219d16c43425edc394d03e0dd15cf058eee430
DasNilima <nilima_sust@yahoo.com>
2024-01-09 13:31:21 -0800

    File & Folder Architecture

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # React-Admin-Dashboard-App
+
+## Code setup & Installation
+create-react-app: https://create-react-app.dev/
+
+material ui: https://mui.com/material-ui/getting-s...
+
+nivo charts: https://nivo.rocks/components
+
+full calendar: https://fullcalendar.io/docs
+
+formik: https://formik.org/docs/overview#inst...
+
+yup validation: https://github.com/jquense/yup
+
+react pro sidebar: https://github.com/azouaoui-med/react...
+
+google fonts: https://fonts.google.com/
---
+++ react-admin/src/scenes/global/Topbar.jsx
@@ -0,0 +1,15 @@
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material";
+import { LightModeOutlinedIcon } from "@mui/icons-material/LightModeOutlinedIcon";
+import { DarkModeOutlinedIcon } from "@mui/icons-material/LightModeOutlinedIcon";
+import { NotificationsOutlinedIcon } from "@mui/icons-material/LightModeOutlinedIcon";
+import { SettingsOutlinedIcon } from "@mui/icons-material/LightModeOutlinedIcon";
+import { PersonOutlinedIcon } from "@mui/icons-material/LightModeOutlinedIcon";
+import { LiglinedIcon } from "@mui/icons-material/LightModeOutlinedIcon";
+const Topbar = () => {
+    return <div>Topbar</div>
+}
+
+export default Topbar;
